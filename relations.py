a0b28560aa00ef7254e3ebc4c00dacdf50ff3d8b
adgsenpai <adg@adgstudios.co.za>
2022-12-07 02:48:14 +0200

    Patched Paper

--- relations.py
+++ relations.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Relations():
     def __init__(self, s, R):
         self.s = s
@@ -37,7 +38,7 @@ class Relations():
     def isTransitive(self):
         M = self.M
         Msquare = np.dot(M, M)
-        #A relation R ⊆ A × A is transitive iff R has the property that for all x, y, z ∈ A, whenever (x, y) ∈ R and (y, z) ∈ R, then (x, z) ∈ R.
+        # A relation R ⊆ A × A is transitive iff R has the property that for all x, y, z ∈ A, whenever (x, y) ∈ R and (y, z) ∈ R, then (x, z) ∈ R.
         for i in range(len(M)):
             for j in range(len(M)):
                 if Msquare[i][j] == 1:
@@ -45,18 +46,17 @@ class Relations():
                         return False
         return True
 
-        
     def isTrichotomy(self):
-        #A relation R on A satisfies the requirement for trichotomy iff, for every x and y
-        #chosen from A such that x ≠ y, we have that x and y are comparable,
-        #i.e. for all x, y ∈ A such that x ≠ y, x R y or y R x (i.e. (x, y) ∈ R or (y, x) ∈ R).
+        # A relation R on A satisfies the requirement for trichotomy iff, for every x and y
+        # chosen from A such that x ≠ y, we have that x and y are comparable,
+        # i.e. for all x, y ∈ A such that x ≠ y, x R y or y R x (i.e. (x, y) ∈ R or (y, x) ∈ R).
         for i in range(len(self.M)):
             for j in range(len(self.M)):
                 if i != j:
                     if self.M[i][j] == 0 and self.M[j][i] == 0:
                         return False
         return True
-                    
+
     def EquivalenceRelation(self):
         if self.isReflexive() and self.isSymmetric() and self.isTransitive():
             return True
@@ -118,11 +118,19 @@ class Relations():
 #s = {0,a,b,ab}
 #R = {(0,{a}),(0,{b}),(0,{a,b}),({a},{b}),({a},{a,b}),({b},{a,b})}
 
+
 # since our class uses numerical values, we need to convert the set to a list of numbers and tuples of ordered pairs
-s = [1,2,3]
+s = [1, 2, 3,4]
 #    0  a  b  ab
-R = [(1,2),(1,3),(2,3)]
-#R = [(1,1),(2,2),(3,3),(4,4),(5,2)]
+
+R = [(1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4)]
 r1 = Relations(s, R)
 r1.printPropertiesOfRelation()
 
+# Output
+# Irreflexive
+# Antisymmetric
+# Transitive
+# Trichotomy
+# Strict Partial Order
+# Strict Total Order
